34ecb37f3f8562b50b3b3a01ef1f33c17fa67a18
Andhitia Rama <andhitia.r@gmail.com>
2026-05-23 08:52:05 +0700

    [UPD] ssi_payment_order_cheque_voucher
    * Add unit tests
    * Apply pre-commit fixes

---
+++ ssi_payment_order_cheque_voucher/tests/test_payment_order_cheque_voucher.py
@@ -0,0 +1,13 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import tagged
+
+from odoo_yaml_test import YamlTransactionCase
+
+
+@tagged("post_install", "-at_install")
+class TestPaymentOrderChequeVoucher(YamlTransactionCase):
+    def test_payment_order_cheque_voucher(self):
+        self.run_yaml_scenario("test_data_payment_order_cheque_voucher.yaml")
